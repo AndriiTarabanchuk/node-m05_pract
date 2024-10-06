dff767f049fdbf535d9b894295b96f875043c331
AndreyTara <explorituse@gmail.com>
2024-10-06 16:07:54 +0300

    'adds-session'

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,9 +1,9 @@
 import createHttpError from 'http-errors';
 import bcrypt from 'bcrypt';
+import { randomBytes } from 'crypto';
 import { UsersCollection } from '../db/models/user.js';
-
-// const ACCESS_TOKEN_LIVE = 1000 * 60 * 15; //15
-// const refreshTokenValidUntil_TOKEN_LIVE = 1000 * 60 * 20; //15
+import { SessionsCollection } from '../db/models/session.js';
+import { ACCESS_TOKEN_LIVE, REFRESH_TOKEN_LIVE } from '../constants/index.js';
 
 export const registerUserService = async (payload) => {
   let user = await UsersCollection.findOne({ email: payload.email }); // check unic email in base
@@ -23,12 +23,24 @@ export const loginUserService = async (payload) => {
   if (!user) {
     throw createHttpError(404, 'User not found!');
   }
+
   const isEqual = await bcrypt.compare(payload.password, user.password);
 
   if (!isEqual) {
     throw createHttpError(401, 'Unauthorized');
   }
+  await SessionsCollection.deleteOne({ userId: user._id });
+
+  const accessToken = randomBytes(30).toString('base64');
+  const refreshToken = randomBytes(30).toString('base64');
+
+  return await SessionsCollection.create({
+    userId: user._id,
+    accessToken,
+    refreshToken,
+    accessTokenValidUntil: new Date(Date.now() + ACCESS_TOKEN_LIVE),
+    refreshTokenValidUntil: new Date(Date.now() + REFRESH_TOKEN_LIVE),
+  });
 };
 
-// export const loginUser = async (req, res) => {};
 // export const logoutUser = async (req, res) => {};
